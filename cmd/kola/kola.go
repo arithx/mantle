f13270a26a1732139c9572d95e27eedd9d063c4e
Stephen Lowrie <stephen.lowrie@gmail.com>
2018-05-14 15:23:50 -0500

    cmd/kola: add Azure

--- cmd/kola/kola.go
+++ cmd/kola/kola.go
@@ -135,6 +135,15 @@ func writeProps() error {
 		AMI          string `json:"ami"`
 		InstanceType string `json:"type"`
 	}
+	type Azure struct {
+		DiskURI   string `json:"diskUri"`
+		Publisher string `json:"publisher"`
+		Offer     string `json:"offer"`
+		Sku       string `json:"sku"`
+		Version   string `json:"version"`
+		Location  string `json:"location"`
+		Size      string `json:"size"`
+	}
 	type DO struct {
 		Region string `json:"region"`
 		Size   string `json:"size"`
@@ -162,6 +171,7 @@ func writeProps() error {
 		Platform string   `json:"platform"`
 		Board    string   `json:"board"`
 		AWS      AWS      `json:"aws"`
+		Azure    Azure    `json:"azure"`
 		DO       DO       `json:"do"`
 		ESX      ESX      `json:"esx"`
 		GCE      GCE      `json:"gce"`
@@ -175,6 +185,15 @@ func writeProps() error {
 			Region:       kola.AWSOptions.Region,
 			AMI:          kola.AWSOptions.AMI,
 			InstanceType: kola.AWSOptions.InstanceType,
+		},
+		Azure: Azure{
+			DiskURI:   kola.AzureOptions.DiskURI,
+			Publisher: kola.AzureOptions.Publisher,
+			Offer:     kola.AzureOptions.Offer,
+			Sku:       kola.AzureOptions.Sku,
+			Version:   kola.AzureOptions.Version,
+			Location:  kola.AzureOptions.Location,
+			Size:      kola.AzureOptions.Size,
 		},
 		DO: DO{
 			Region: kola.DOOptions.Region,
